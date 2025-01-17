5bdb1da85502018c4eeff9789fd097c41e70aabc
Kriti Chopra <37541206+Kriti-Chopra@users.noreply.github.com>
2025-01-17 16:08:45 +0530

    Added assertions

--- Hovers.java
+++ Hovers.java
@@ -5,33 +5,35 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
 
 public class Hovers {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
-		//https://the-internet.herokuapp.com/hovers
-		//div[@class='figure']//img
-		//div[@class='figcaption']//h5
 		
 		String url = "https://the-internet.herokuapp.com/hovers";
-		By figureBy = By.cssSelector("div.figure img");
-		By figureCaptionBy =By.cssSelector("div.figcaption h5");
+		By figureBy = By.xpath("//div[@class='figure']//img");
+		By figureCaptionBy =By.xpath("//div[@class='figcaption']//h5");
 		
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		
 		//Access the page
 		driver.get(url);
-		
+	
 		//hover over the images and get text
 		List<WebElement> figuresList = driver.findElements(figureBy);
-		for(WebElement figure:figuresList) {
+		List<WebElement> captionsList = driver.findElements(figureCaptionBy);
+		for(int i=0;i<figuresList.size();++i) {
+			Assert.assertFalse(captionsList.get(i).isDisplayed());
 			Actions actions = new Actions(driver);
-			actions.moveToElement(figure).build().perform();
-			
-			
+			actions.moveToElement(figuresList.get(i)).build().perform();
+			Assert.assertTrue(captionsList.get(i).isDisplayed());
+			System.out.println(captionsList.get(i).getText());
 		}
+		
+		driver.quit();
 		
 	}
 
